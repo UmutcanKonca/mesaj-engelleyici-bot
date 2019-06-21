ef56f2680547a137a0977eacb661b1f80df94abf
ardasoyturk <fake4rd4@gmail.com>
2019-06-21 15:11:18 +0300

    Örnek ayarlar dosyası eklendi.

---
+++ config.example.js
@@ -0,0 +1,5 @@
+module.exports = {
+    blocked: ['amk', 'sikik', 'https://nakedphotos.club'], // Engellenen kelimeleri buraya giriyoruz.
+    what2do: 'sil', // Engellenen kelimeyi attığında ne yapacağını seçiyoruz. Seçenekler: ban, kick, sil
+    token: 'NTEzMjc1NDE0NDIxMDQ1MjQ4.XQzFaw.IPyveESXyCIVoTT1RADMGdUnuPY' // Botun süpersonik tokeni, token çalışmıyor bu arada uğraşmayın direkt. 
+}
